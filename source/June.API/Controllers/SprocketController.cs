4e017ff39a232c0a6a9b854eb50b8d82d1a525c6
Michael Bradvica <206864961@uniparks.com>
2025-12-17 14:29:55 -0500

    updated config

--- source/June.API/Controllers/SprocketController.cs
+++ source/June.API/Controllers/SprocketController.cs
@@ -2,10 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace June.API.Controllers
 {
+    /// <inheritdoc />
     [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    [Route("sprocket")]
+    public class SprocketController : ControllerBase
     {
+        /// <summary>
+        /// Gets my sprocket.
+        /// </summary>
+        /// <returns>A <see cref="IActionResult"/>.</returns>
         [HttpGet]
         [ProducesResponseType<string>(StatusCodes.Status200OK)]
         public IActionResult Get()
--- source/June.API/Program.cs
+++ source/June.API/Program.cs
@@ -1,15 +1,29 @@
 
+using System.Reflection;
+
 namespace June.API
 {
+    /// <summary>
+    /// Entry point for the application.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Application entry point.
+        /// </summary>
+        /// <param name="args">A list of command line arguments.</param>
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllers();
             builder.Services.AddOpenApi();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
 
             var app = builder.Build();
 
